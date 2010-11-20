839ac2a99c818e906a128da5d98e06285b425527
Ben Noordhuis <info@bnoordhuis.nl>
2010-11-20 16:43:43 +0100

    Rework, this and that, one step closer, etc.

--- src/ngx_http_auth_cas_module.c
+++ src/ngx_http_auth_cas_module.c
@@ -2,44 +2,53 @@
 #include <ngx_core.h>
 #include <ngx_http.h>
 
-static void *create_main_conf(ngx_conf_t *cf) {
-	return NULL;
+typedef struct {
+	ngx_str_t cas_login_url;
+} ngx_http_auth_cas_ctx_t;
+
+static void *ngx_http_auth_cas_create_loc_conf(ngx_conf_t *cf) {
+	ngx_http_auth_cas_ctx_t *ctx = ngx_pcalloc(cf->pool, sizeof(*ctx));
+	ngx_str_null(&ctx->cas_login_url);
+	return ctx;
 }
 
-static char *init_main_conf(ngx_conf_t *cf, void *conf) {
-	return NULL;
+static char *ngx_http_auth_cas_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child) {
+	const ngx_http_auth_cas_ctx_t *prev = (ngx_http_auth_cas_ctx_t *) parent;
+	ngx_http_auth_cas_ctx_t *conf = (ngx_http_auth_cas_ctx_t *) child;
+
+	if (conf->cas_login_url.data == NULL) {
+		conf->cas_login_url = prev->cas_login_url;
+	}
+
+	return NGX_CONF_OK;
 }
 
-static char *cas_login_url(ngx_conf_t *cf, ngx_command_t *cmd, void *conf) {
-	return NULL;
-}
-
-static ngx_int_t postconfiguration(ngx_conf_t *cf) {
+static ngx_int_t ngx_http_auth_cas_init(ngx_conf_t *cf) {
 	return NGX_OK;
 }
 
 static ngx_command_t commands[] = {
 	{
-		.name   = ngx_string("cas_login_url"),
-		.type   = NGX_MAIN_CONF | NGX_CONF_TAKE1,
-		.set    = cas_login_url,
-		.conf   = NGX_MAIN_CONF,
-		.offset = 0,
-		.post   = NULL
+		ngx_string("cas_login_url"),
+		NGX_HTTP_MAIN_CONF |NGX_HTTP_SRV_CONF | NGX_HTTP_LOC_CONF | NGX_HTTP_LMT_CONF | NGX_CONF_TAKE1,
+		ngx_conf_set_str_slot,
+		NGX_HTTP_LOC_CONF_OFFSET,
+		offsetof(ngx_http_auth_cas_ctx_t, cas_login_url),
+		NULL
 
 	},
 	ngx_null_command
 };
 
 static ngx_http_module_t ctx = {
-	.preconfiguration  = NULL,
-	.postconfiguration = postconfiguration,
-	.create_main_conf  = create_main_conf,
-	.init_main_conf    = init_main_conf,
-	.create_srv_conf   = NULL,
-	.merge_srv_conf    = NULL,
-	.create_loc_conf   = NULL,
-	.merge_loc_conf    = NULL,
+	NULL,
+	ngx_http_auth_cas_init,
+	NULL,
+	NULL,
+	NULL,
+	NULL,
+	ngx_http_auth_cas_create_loc_conf,
+	ngx_http_auth_cas_merge_loc_conf
 };
 
 ngx_module_t ngx_http_auth_cas_module = {
